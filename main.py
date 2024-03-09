5f2c7019caef66ae49ddf8781a3bf81f0abf699d
nidhal karchoud <114624188+yar01110@users.noreply.github.com>
2024-03-09 17:06:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import cv2
 import cvzone
 from face_detect import detect 
-from face_reco_cossim import recognize_face
-while True:
-    
+from face_reco_cossim import recognize_face 
+
+def main():
     cap = cv2.VideoCapture(0)
 
     while cap.isOpened():
-        
         ret, frame = cap.read()
         if not ret:
             break
 
         mainframe = frame.copy()
+        face_result = detect(mainframe)
 
-        face_result=detect(mainframe)
         for info in face_result:
             parameters = info.boxes
             for box in parameters:
                 x1, y1, x2, y2 = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                name=recognize_face(mainframe[y1:y2, x1:x2],0.6)
                 h, w = y2 - y1, x2 - x1
-                
+
+                if h < 80 or w < 80:
+                    break
+
+                name = recognize_face(mainframe[y1:y2, x1:x2], 0.6)
+
                 cvzone.cornerRect(mainframe, [x1, y1, w, h], l=9, rt=3)
                 cvzone.putTextRect(mainframe, name, [x1, y1 - 10])
                 cvzone.putTextRect(mainframe, 'Input Video', [0, 36])
@@ -34,3 +37,6 @@ while True:
 
     cap.release()
     cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
